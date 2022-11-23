e5fa3fc0f99be3e03d3989d00d3768a4b45cf706
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-11-23 12:31:58 -0500

    add: about module

---
+++ src/components/About/About.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import styles from './About.module.css';
+
+const description = 'Me gusta programar tanto como los juegos.\nSoy un desarrollador de software con ganas de aprender todos los días algo nuevo y tener cosas que hacer a toda hora.\n\nCuento con experiencia en multiples lenguajes como:';
+
+const langs = ['JavaScript', 'TypeScript', 'HTML', 'Java', 'Rust', 'SQL', 'C#', 'Python', 'C++'];
+
+const About = () => {
+
+    useEffect(() => {
+        const desc = document.querySelector(`#d-text`);
+        desc.innerHTML
+            = description.replaceAll('\n', '<br />')
+    }, []);
+
+    return (
+        <section className={styles.main}>
+            <h1>About me</h1>
+            <div className={styles.description}>
+                <p id='d-text'></p>
+                <ul>
+                    {langs.map((l, i) => (
+                        <li key={i}>{l}</li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default About;
