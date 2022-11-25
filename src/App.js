1a4f217c58b1d68670bdd46dc39047ff9386822d
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-11-25 16:02:52 -0500

    add: title website and translates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Projects from './components/Projects/Projects';
+import translate from './data/translate';
+
+document.title = translate('WEB_TITLE');
 
 function App() {
     return (
