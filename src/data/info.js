aea3e115305042dc31fbdcbae3990ad0533e908c
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-11-24 21:54:25 -0500

    add: info projects

---
+++ src/data/info.js
@@ -0,0 +1,32 @@
+export const PROJECTS = [
+    {
+        name: 'Portfolio Web',
+        description: 'Source code of my web portfolio',
+        repository: 'https://github.com/GalassoX/portafolio',
+    },
+    {
+        name: 'To-Do List app',
+        description: 'To-Do mini app made in Rust to learn',
+        repository: 'https://github.com/GalassoX/rusty-todo-list-app'
+    },
+    {
+        name: 'Rootwide API',
+        description: 'RestFUL API made in TypeScript for a gaming community that currently doesn\'t exist anymore, the API was made entirely by me, it uses the following modules: express, mysql, JWT, bcrypt.js',
+        repository: 'https://github.com/GalassoX/rootwide-apirest'
+    },
+    {
+        name: 'Rootwide Website',
+        description: 'The Rootwide API website, this website (like the API) is abandoned and outdated, it is made with React.js and JavaScript.',
+        repository: 'https://github.com/GalassoX/rootwide-website'
+    },
+    {
+        name: 'Tic Tac Toe console',
+        description: 'C++ game, made to learn',
+        repository: 'https://github.com/GalassoX/tic-tac-toe'
+    },
+    {
+        name: 'Music Player',
+        description: 'Made to learn React (outdated)',
+        repository: 'https://github.com/GalassoX/Music-Player-App'
+    }
+];
