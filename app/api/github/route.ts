7fe7e105ca0ae4d18df70a0a28d2bd77cf044219
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-05-09 17:45:22 -0500

    refactor github fetch data for performance

--- app/api/github/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { octokit } from "./github";
-
-export async function GET(request: NextRequest) {
-    const response = await octokit.request('GET /users/{username}', {
-        username: 'GalassoX',
-    });
-    return NextResponse.json(response.data)
-}
