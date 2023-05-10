6d9dcda9b2151e51d45ecfe447cdf180a728457e
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-05-09 19:44:48 -0500

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const robotoFont = Roboto({ weight: "400", subsets: ["latin"] })
 
 export const metadata = {
-  title: 'Rafael Marrugo - Portafolio Web',
-  description: 'Portafolio Web made in Next.js 13',
+  title: 'Rafael Marrugo - Portfolio Web',
+  description: 'Hi! Im Rafael Marrugo, this is my Portfolio Web. Enter to view more info about me!',
 }
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={robotoFont.className}>{children}</body>
     </html>
   )
 }
