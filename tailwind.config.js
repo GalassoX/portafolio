3e998845b9f79fdb0056bee862aedffda0409088
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-05-10 15:53:57 -0500

    use tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        port: {
+          blueground: {
+            100: "#3B9090",
+            200: "#1A8585",
+            300: "#335858"
+          },
+          font: "#fef2c1"
+        }
+      }
     },
   },
   plugins: [],
