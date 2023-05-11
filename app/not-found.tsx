81fa97c18efb4957c70a2d4d7d035c0d08e2e2c7
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-05-10 23:34:50 -0500

    remake 404 view

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
-import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.main}>
-      <h1>Pagina no encontrada</h1>
-      <Link href="/">Volver a la pagina principal</Link>
-    </div>
+    <main className='h-screen flex justify-center items-center bg-port-blueground-300 text-white'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold mb-4 p-2'>¿Estás un poco curioso, no?</h1>
+        <Link href="/" className="underline text-lg italic font-bold text-port-font">Perdon, quiero volver a la pagina principal</Link>
+      </div>
+    </main>
   )
 }
