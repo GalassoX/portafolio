8fad16bb980f18396c8084668b5778747b438e7a
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-05-10 23:25:13 -0500

    update socials urls

--- data/socials.ts
+++ data/socials.ts
@@ -1,7 +1,9 @@
 const SOCIALS = {
     twitter: 'https://twitter.com/x_galasso/',
     github: 'https://github.com/GalassoX/',
-    linkedin: 'https://www.linkedin.com/in/rafael-marrugo/'
+    linkedin: 'https://www.linkedin.com/in/rafael-marrugo/',
+    cv: 'https://drive.google.com/file/d/1Mqe4I6POcNe7x2nLxBP9RygphydutnmE/view?usp=sharing',
+    mail: 'mailto:rafaelmarrugo28@gmail.com'
 }
 
 export default SOCIALS;
